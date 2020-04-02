df0becf5c1635dea2f6fbc6fdc51839588c76293
ludvigalden <aldenludvig@gmail.com>
2020-04-02 17:30:33 +0200

    remove initAceno declaration

--- src/bin/aceno.ts
+++ src/bin/aceno.ts
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
-import { initAceno } from "../aceno.init";
 import { ScriptMap, Cli, findScripts } from "../cli";
 import { Options } from "../options";
+import { Tsconfig } from "../file";
 
 export async function aceno() {
-    await initAceno();
+    const tsconfig = await Tsconfig.read();
+
+    if (tsconfig) {
+        const processTsconfig = Options.cwd !== process.cwd() && tsconfig ? tsconfig.relativize(process.cwd()) : tsconfig;
+
+        await import("ts-node").then((tsNode) => {
+            return tsNode.register({
+                transpileOnly: true,
+                compilerOptions: processTsconfig.current.compilerOptions,
+                skipProject: true,
+            });
+        });
+
+        if (Object.keys(processTsconfig.current.compilerOptions.paths).length) {
+            await import("tsconfig-paths").then((tsconfigPaths) => {
+                tsconfigPaths.register({
+                    baseUrl: processTsconfig.current.compilerOptions.baseUrl || "./",
+                    paths: processTsconfig.current.compilerOptions.paths,
+                });
+            });
+        }
+    }
 
     const scriptMap = await new Promise<Map<string, () => Promise<void>>>(async (resolve) => {
         const found = new ScriptMap();
